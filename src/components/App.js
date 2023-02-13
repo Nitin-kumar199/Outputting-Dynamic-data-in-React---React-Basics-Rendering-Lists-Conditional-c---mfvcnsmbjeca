d7a6440be4966a22eacc98c22301061749eeb94b
Nitin-kumar199 <112743746+Nitin-kumar199@users.noreply.github.com>
2023-02-13 16:31:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
-
 const App = () => {
   const [name, setName] = useState('');
 
