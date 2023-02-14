454f08a8be7fef39c71cb8afde0e7447f9303d1f
Nitin-kumar199 <112743746+Nitin-kumar199@users.noreply.github.com>
2023-02-14 10:29:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,18 @@ import '../styles/App.css';
 const App = () => {
   const [name, setName] = useState("____");
   const [input, setInput] = useState("");
-
+   const handleButtonClick = () => {
+   setName(input);
+  };
   const handleInputChange = (event) => {
     setInput(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-   setName(input);
   };
 
   return (
     <div id="main">
       <input id='input' onChange={handleInputChange}></input>
       <button id='button' onClick={handleButtonClick}>Click</button>
-      <p id='text'>Hello my name is ____ and I study at Newton School</p>
+      <p id='text'>Hello my name is {name} and I study at Newton School</p>
     </div>
   )
 }
