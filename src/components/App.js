67d410e9eddd14118d40a961da73c1ae648f72f1
Nitin-kumar199 <112743746+Nitin-kumar199@users.noreply.github.com>
2023-02-16 23:19:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
-  const [name, setName] = useState("_____");
-  const [input, setInput] = useState("");
-   const handleButtonClick = (e) => {
-   setName(input);
-  };
-  const handleInputChange = (event) => {
-    setInput(event.target.value);
-  };
-
+const [name,setName]=useState("_____");
+const [input,setInput]=useState("");
+const onButtonClick=(e)=>{
+setName(input)
+}
+const onInputChange=(e)=>{
+setInput(e.target.value)
+}
   return (
     <div id="main">
-      <input id='input' onChange={handleInputChange}></input>
-      <button id='button' onClick={handleButtonClick}>Click</button>
-      <p id='text'>Hello my name is {name} and I study at Newton School</p>
+      <input id='input' onChange={onInputChange}></input>
+      <button id='button' onClick={onButtonClick}>Click</button>
+      <p id='text'> Hello my name is {name} and I study at Newton School</p>
     </div>
   )
 }
-
 export default App;
